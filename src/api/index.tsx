14d08374f029c059e4fd6b6bcb91d66f27684f38
Hennadii_Mariiev <gennadiy.mariev@devit.group>
2023-01-24 12:30:46 +0200

    [env-example, gitignore, api] => remove api-key from api, add env-file and fix config

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import IQuoteResponse from "../interfaces/response";
 
 const URL = 'https://quotes15.p.rapidapi.com/quotes/random/'
-const API_KEY = "4dce3c46edmshd7ac5b0245fcad9p14d63ejsnf00486cd2816";
+const API_KEY = import.meta.env.VITE_API_KEY;
 const HOST = "quotes15.p.rapidapi.com'"
 
 const instance = axios.create({
